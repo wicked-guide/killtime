a01a39a1d41353c3d0058fff9f3f1307bae0e58a
wandermap <wis.with.witch@gmail.com>
2022-03-08 00:23:13 +0900

    トップに学部・職業を追加

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <b-container fluid class="bv-example-row">
       <b-row class="py-3">
         <!-- main -->
-        <b-col cols="8">
+        <b-col md="8">
           <b-form-input
             size="sm"
             type="search"
@@ -31,7 +31,7 @@
           </section>
         </b-col>
         <!-- sub -->
-        <b-col cols="4" class="w-md right-0 mh-100 overflow-auto pb-5">
+        <b-col md="4" class="w-md right-0 mh-100 overflow-auto pb-5">
           <!-- game -->
           <section class="bg-green-100 p-2 rounded-2xl">
             <nuxt-link to="game">
@@ -40,14 +40,14 @@
             /></nuxt-link>
           </section>
 
-          <!-- 進路紹介 -->
+          <!-- ちょっとした情報 -->
           <section class="bg-green-100 mt-3 p-2 rounded-2xl">
             <div class="text-2xl">ちょっとした情報</div>
             <ul>
               <li
                 v-for="(i, index) in info"
                 :key="index"
-                class="bg-white p-1 mb-1"
+                class="bg-white p-1 mb-1 rounded-tr-2xl"
               >
                 <nuxt-link to="info">
                   <div class="text-indigo-700">{{ i.title }}</div>
@@ -58,16 +58,57 @@
           </section>
 
           <!-- ミッション -->
-          <section class="bg-green-100 mt-3 p-2 rounded-2xl mb-5">
-            <div class="text-2xl">依頼ミッション</div>
+          <section class="bg-green-100 mt-3 p-2 rounded-2xl">
+            <div class="text-2xl">クエスト</div>
             <ul>
-              <li class="bg-white p-1 mb-1">
+              <li class="bg-white p-1 mb-1 rounded-tr-2xl">
                 <nuxt-link to="mission">
                   <div class="text-indigo-700">誰か助けて！</div>
                   <div class="text-gray-400 text-md-right">2023/10/10</div>
                 </nuxt-link>
               </li>
+              <li class="bg-white p-1 mb-1 rounded-tr-2xl">
+                <nuxt-link to="mission">
+                  <div class="text-indigo-700">
+                    地球から月までの距離を、自分で実際に測ってみたい
+                  </div>
+                  <div class="text-gray-400 text-md-right">2023/9/17</div>
+                </nuxt-link>
+              </li>
             </ul>
+          </section>
+
+          <!-- 学問紹介 -->
+          <section class="bg-green-100 mt-3 p-2 rounded-2xl mb-5">
+            <div class="text-2xl">やりたいこと探し</div>
+            <section class="bg-blue-200 rounded-tr-2xl p-1">
+              <div class="text-center">進学：学部選び編</div>
+              <ul class="px-1">
+                <li class="bg-white p-1 mb-1 rounded-tr-2xl">
+                  <nuxt-link to="mission">
+                    <div class="text-indigo-700">謎の学問？哲学部</div>
+                    <div class="text-gray-400 text-md-right">2023/9/10</div>
+                  </nuxt-link>
+                </li>
+                <li class="bg-white p-1 mb-1 rounded-tr-2xl">
+                  <nuxt-link to="mission">
+                    <div class="text-indigo-700">実験漬けの毎日　科学部</div>
+                    <div class="text-gray-400 text-md-right">2023/9/10</div>
+                  </nuxt-link>
+                </li>
+              </ul>
+            </section>
+            <section class="bg-blue-200 rounded-tr-2xl mt-2 p-1">
+              <div class="text-center">就職：仕事選び編</div>
+              <ul class="px-1">
+                <li class="bg-white rounded-tr-2xl p-1 mb-1">
+                  <nuxt-link to="mission">
+                    <div class="text-indigo-700">手堅い人気！公務員の実態</div>
+                    <div class="text-gray-400 text-md-right">2023/8/12</div>
+                  </nuxt-link>
+                </li>
+              </ul>
+            </section>
           </section>
         </b-col>
       </b-row>
